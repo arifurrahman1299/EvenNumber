87e5824849ea0e252f6ab69d02e77a13931099db
Arifur Rahman <arifurrahman1299@gmail.com>
2021-02-07 11:59:26 +0600

    Task 1

---
+++ EvenNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EvenNumber
+{
+    static class Program
+    {
+
+        static void Main()
+        {
+
+            for (int number = 2; number <= 50; number++)
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine(number);
+                }
+            Console.ReadLine();
+        }
+    }
+}
